a573bf139329752304d0cb5749b9e19143c14545
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-07-17 16:42:51 -0700

    used try with resources

--- src/main/java/springLearning/Learnspringframework/helloWorld/app02HelloWorldSpring.java
+++ src/main/java/springLearning/Learnspringframework/helloWorld/app02HelloWorldSpring.java
@@ -7,23 +7,25 @@ import java.util.Arrays;
 public class app02HelloWorldSpring {
     public static void main(String[] args) {
         //1:launch a spring context
-        var context =new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-        //2:configure the things that we want spring framework to manage-@Configuration
-        //HelloWorldConfiguration -@Configration
-        //name :@Bean
+        try(var context =new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);){
+            //2:configure the things that we want spring framework to manage-@Configuration
+            //HelloWorldConfiguration -@Configration
+            //name :@Bean
 
 
-        //3: Retrieving Beans managed by spring
-        System.out.println(context.getBean("name"));
-        System.out.println(context.getBean("age"));
-        System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("add2"));
-        System.out.println(context.getBean("person2MethodCall"));
-        System.out.println(context.getBean("person3Parameters"));
-        System.out.println(context
-                .getBean(Person.class));
-        System.out.println(context.getBean(Address.class));
+            //3: Retrieving Beans managed by spring
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            System.out.println(context.getBean("person"));
+            System.out.println(context.getBean("add2"));
+            System.out.println(context.getBean("person2MethodCall"));
+            System.out.println(context.getBean("person3Parameters"));
+            System.out.println(context
+                    .getBean(Person.class));
+            System.out.println(context.getBean(Address.class));
 //        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+
 
     }
 }
