f6bd3102d35a13f1c832e09b2cbad948f2b608bf
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-07-17 15:57:33 -0700

    first commit day 2

--- src/main/java/springLearning/Learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/springLearning/Learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package springLearning.Learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name,int age,Address add){};
+record Person(String name,int age,Address addd){};
 
 //Eliminate verbosity in creating a java Beans
 //Public accesor methods, constructor
@@ -24,25 +24,26 @@ public class HelloWorldConfiguration {
 
 @Bean
     public Person person(){
-        return new Person("mani",23,new Address("bkjb","khv"));
+        return new Person("mani",23,new Address("smk","khv"));
 }
 
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(),age(), addd());
+    }
+
 @Bean(name = "add2")
-    public Address add(){
+    public Address addd(){
       return new Address("Dharmapuri","Hyderabad");
 }
 
-@Bean
-    public Person person2MethodCall(){
-        return new Person(name(),age(), add());
-}
 
 @Bean
-    public Person person3Parameters(String name,int age,Address add){
-        return new Person(name(),age(), add());
+    public Person person3Parameters(String name,int age,Address add3){
+        return new Person(name,age, add3);
     }
-    @Bean(name = "add2")
-    public Address add(){
-        return new Address("Dharmapuri","Hyderabad");
+    @Bean(name = "add3")
+    public Address ad3(){
+        return new Address("dmp","Hyderabad");
     }
 }
--- src/main/java/springLearning/Learnspringframework/app02HelloWorldSpring.java
+++ src/main/java/springLearning/Learnspringframework/app02HelloWorldSpring.java
@@ -17,8 +17,9 @@ public class app02HelloWorldSpring {
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
         System.out.println(context.getBean("add2"));
-        System.out.println(context.getBean(Address.class));
         System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+//        System.out.println(context.getBean(Address.class));
 
     }
 }
