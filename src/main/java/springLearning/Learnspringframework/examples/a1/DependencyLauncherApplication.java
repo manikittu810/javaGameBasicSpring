24918285b93c521ac930928a3aa7d170db06c095
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-07-18 17:27:21 -0700

    contructor  injection

--- src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
+++ src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
@@ -14,17 +14,25 @@ Dependency1 dependency1;
     @Autowired //Field injection. it is performed as soon as you write @Autowired on the Dependencies
 Dependency2 dependency2;
 
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter Injection -setDependency1");
-        this.dependency1 = dependency1; //Setter injection
+//    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor injection -> YourBusinessClass"); //Constructor injection  and @Autowired is not mandatory
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
     }
 
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter Injection -setDependency2");
-        this.dependency2 = dependency2; //setter injection
-    }
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection -setDependency1");
+//        this.dependency1 = dependency1; //Setter injection
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter Injection -setDependency2");
+//        this.dependency2 = dependency2; //setter injection
+//    }
 
     public String toString(){
     return "Using  " + dependency1 + " and  " + dependency2;
