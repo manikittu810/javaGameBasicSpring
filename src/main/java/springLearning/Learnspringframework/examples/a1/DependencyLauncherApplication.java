75075a43a05433504bc36faacb4cb1d5433d9c5c
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-07-18 17:15:03 -0700

    setter injection

--- src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
+++ src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
@@ -13,7 +13,20 @@ class  YourBusinessClass{
 Dependency1 dependency1;
     @Autowired //Field injection. it is performed as soon as you write @Autowired on the Dependencies
 Dependency2 dependency2;
-public String toString(){
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Injection -setDependency1");
+        this.dependency1 = dependency1; //Setter injection
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection -setDependency2");
+        this.dependency2 = dependency2; //setter injection
+    }
+
+    public String toString(){
     return "Using  " + dependency1 + " and  " + dependency2;
 }
 }
