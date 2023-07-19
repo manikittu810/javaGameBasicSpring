eb2d4d4adaac820979c4e0c00d991f2c84daa30f
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-07-18 17:03:12 -0700

    Field Dependency injection without using any constructors or Setters. Here depenedency injection is done by using reflection.

--- src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
+++ src/main/java/springLearning/Learnspringframework/examples/a1/DependencyLauncherApplication.java
@@ -1,5 +1,6 @@
 package springLearning.Learnspringframework.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,13 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 @Component
 class  YourBusinessClass{
-
+    @Autowired
+Dependency1 dependency1;
+    @Autowired //Field injection. it is performed as soon as you write @Autowired on the Dependencies
+Dependency2 dependency2;
+public String toString(){
+    return "Using  " + dependency1 + " and  " + dependency2;
+}
 }
 @Component
 class Dependency1{
@@ -26,7 +33,7 @@ public class DependencyLauncherApplication {
         try (var context =
                      new AnnotationConfigApplicationContext(DependencyLauncherApplication.class)) {
             Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-
+            System.out.println(context.getBean(YourBusinessClass.class));
         }
 
     }
